9d54a1a366d6766bf76eb20ac249cbe76ad6b198
Steven <steven@stvnx.nl>
2023-02-12 20:25:35 +0100

    TSKS > Sidebar WIP

--- components/Sidebar/Sidebar.vue
+++ components/Sidebar/Sidebar.vue
@@ -14,7 +14,7 @@
                 stroke-width="1.5"
                 stroke="currentColor" 
                 :class="{'mr-2': store.showSidebar}"
-                class="w-8 h-8 mt-1"
+                class="min-w-[32px] h-8 mt-1"
             >
                 <path 
                     stroke-linecap="round"
@@ -22,7 +22,9 @@
                     d="M9 4.5v15m6-15v15m-10.875 0h15.75c.621 0 1.125-.504 1.125-1.125V5.625c0-.621-.504-1.125-1.125-1.125H4.125C3.504 4.5 3 5.004 3 5.625v12.75c0 .621.504 1.125 1.125 1.125z"
                 />
             </svg>
-            <span v-if="store.showSidebar">
+            <span 
+                v-if="store.showSidebar"
+            >
                 tsks
             </span>
         </h1>
--- components/Sidebar/components/CreateProject.vue
+++ components/Sidebar/components/CreateProject.vue
@@ -1,6 +1,21 @@
 <script setup lang="ts">
+    import { useSidebarStore } from '~~/stores/sidebarStore';
+
+    const store = useSidebarStore()
     const state = reactive({
         showModal: false
+    })
+
+    const buttonText = computed(() => {
+        if(store.showSidebar) {
+            return 'Create project'
+        } else {
+            return `
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="min-w-[32px] w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+            `
+        }
     })
 
     const toggleProjectModal = () => {
@@ -10,8 +25,32 @@
 
 <template>
     <li class="mt-4">
-        <button @click="toggleProjectModal">
-            Create project
+        <button
+            class="flex items-center"
+            @click="toggleProjectModal"
+        >
+            <svg
+                xmlns="http://www.w3.org/2000/svg" 
+                fill="none" 
+                viewBox="0 0 24 24" 
+                stroke-width="1.5" 
+                stroke="currentColor" 
+                class="min-w-[24px] w-6 h-6 mr-2"
+            >
+                <path 
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M12 4.5v15m7.5-7.5h-15"
+                />
+            </svg>
+            <transition>
+                <span
+                    v-if="store.showSidebar"
+                    class="whitespace-nowrap"
+                >
+                    Create project
+                </span>
+            </transition>
         </button>
     </li>
     <div 
